c3391246aef5235230c28b96f51885a1f16d4894
Your Name <you@example.com>
2022-07-05 17:20:51 +0530

    this is PR commit

---
+++ src/com/java/prc/Git.java
@@ -0,0 +1,17 @@
+package com.java.prc;
+
+public class Git {
+
+	public void gitcmd() {
+		
+		System.out.println("this is git cmd terminal ");
+		
+	}
+   
+	public static void main(String[] args) {
+		
+		Git g= new Git();
+		g.gitcmd();
+		
+	}
+}
---
+++ src/com/java/prc/PullRequest.java
@@ -0,0 +1,20 @@
+package com.java.prc;
+
+public class PullRequest {
+
+	public void PR() {
+		System.out.println("this is pull request from shridhar to Ankush for code review ");
+		
+	}
+
+	
+	public static void main(String [] args) {
+		
+		PullRequest p = new PullRequest();
+		p.PR();
+		
+		
+	}
+	
+	
+}
